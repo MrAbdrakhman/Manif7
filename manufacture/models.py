c81fd8f30372266b42a7dc1d115c910d0f798563
Emirlan <you@example.com>
2022-08-04 13:57:05 +0600

    «emirlan»

--- manufacture/models.py
+++ manufacture/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Sum
 from django_pandas.managers import DataFrameManager
+from django.contrib.auth.models import AbstractUser
 
 
 class Sale(models.Model):
@@ -151,5 +152,14 @@ class DailyProduction(models.Model):
 
     def defect_sum(self):
         return (self.defect_machine + self.defect_worker + self.defect_saya) * 200  # брак по 200 сом за 1 брак
+
+
+
+class User(AbstractUser):
+    is_accountant = models.BooleanField(default=False)
+    is_cashier = models.BooleanField(default=False)
+    new_password = models.CharField(max_length=100)
+
+
 
 
--- manufacture/urls.py
+++ manufacture/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('', index, name='index'),
@@ -22,4 +23,7 @@ urlpatterns = [
     path('search_month', search_monthly, name='search_month'),
     path('monthly_production2', search_monthly, name='monthly_production2'),
     path('pandas_report', pandas_view, name='pandas_report'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', user_login, name='login'),
+    path('register_new/', register_new, name='register_new'),
 ]
